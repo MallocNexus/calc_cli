8e5ac3f52b7621dda91cf67ecfac27f00860a797
marthinusswart <marthinus.swart@gmail.com>
2026-06-15 21:33:06 +1000

    latest

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,21 @@
 #include <iostream>
 #include <string>
 #include <unistd.h>
+#include <cstdio>
 
 #include <ftxui/component/screen_interactive.hpp>
 
-static int RunHeadless(const std::string& expr) {
+static int RunHeadless(const std::string& expr, bool print_only_value) {
     std::string formatted = util::FormatExpression(expr);
     Calculator calc;
     EvaluationResult res = calc.Evaluate(formatted);
     
     if (res.ok) {
-        std::cout << formatted << " = " << util::FormatDouble(res.value) << "\n";
+        if (print_only_value) {
+            std::cout << util::FormatDouble(res.value) << "\n";
+        } else {
+            std::cout << formatted << " = " << util::FormatDouble(res.value) << "\n";
+        }
         return EXIT_SUCCESS;
     } else {
         std::cerr << "Error: " << res.error << "\n";
@@ -26,18 +31,35 @@ static int RunHeadless(const std::string& expr) {
 }
 
 int main(int argc, char* argv[]) {
+    bool print_only_value = false;
+    std::string expr_arg = "";
+
+    // Parse arguments
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg == "--stdin-file" && i + 1 < argc) {
+            std::freopen(argv[i + 1], "r", stdin);
+            i++; // skip file path
+        } else if (arg == "--value") {
+            print_only_value = true;
+        } else if (arg == "--expr" && i + 1 < argc) {
+            expr_arg = argv[i + 1];
+            i++; // skip expression value
+        }
+    }
+
     // 1. Check for piped input (stdin is not a terminal)
     if (!isatty(STDIN_FILENO)) {
         std::string expr;
         std::getline(std::cin, expr);
         if (!expr.empty()) {
-            return RunHeadless(expr);
+            return RunHeadless(expr, print_only_value);
         }
     }
 
     // 2. Explicit headless mode via arguments
-    if (argc == 3 && std::string(argv[1]) == "--expr") {
-        return RunHeadless(argv[2]);
+    if (!expr_arg.empty()) {
+        return RunHeadless(expr_arg, print_only_value);
     }
     using namespace ftxui;
 
